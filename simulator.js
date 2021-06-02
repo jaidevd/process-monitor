fe7fecadac3b0a10329adf7bfb2675e99dc46c4b
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2021-06-02 11:14:45 +0530

    WIP

--- simulator.js
+++ simulator.js
@@ -1,5 +1,16 @@
 /* globals g1 */
 
+const EDIT_OPTS = {
+  'material-a':
+    '<select id="material-a" class="form-control svgin"><option value="A1">A1</option><option value="A2">A2</option></select>',
+  'material-b': '<select id="material-b" class="form-control svgin"><option value="B1">B1</option><option value="B2">B2</option></select>',
+  solvent: '<select id="solvent" class="form-control svgin"><option value="Low">Low</option><option value="Hi">Hi</option></select>',
+  reactor: '<input id="reactor-speed" type="range" min="80" max="86" value="83" step="1"  class="form-control svgin">',
+  dryer: '<input id="dryer-speed" type="range" min="20" max="25" value="22" step="1"  class="form-control svgin">',
+  crusher: '<input id="crusher-speed" type="range" min="12" max="15" value="14" step="1"  class="form-control svgin">',
+  crystallizer: '<input id="crystallizer-speed" type="range" min="10" max="15" value="12" step="1"  class="form-control svgin">'
+}
+
 $(function () {
   var $template = $('script[type="text/html"]')
   var url = g1.url.parse(location.href)
@@ -38,13 +49,37 @@ $(function () {
       return path
     }
   }
+  function render_popover_content() {
+    let html = EDIT_OPTS[this.className.baseVal]
+    return html
+  }
+
+  function render_popover_title() {
+    let param = $(this).attr('data-param')
+    return "Change " + param
+  }
 
   if ($template.length)
     $.getJSON($template.data('src') || 'data').done(function (data) {
       $('.loading').remove()
       var index = q.time ? +q.time - 1 : 0
       render()
-
+      $('[data-toggle="popover"]').on('shown.bs.popover', function(evt) {
+        $('.svgin').change(function() {
+          let param_id = $(this).attr('id')
+          if (param_id == "solvent") {
+            $('#solvent-' + $(this).val().toLowerCase()).attr('checked', true)
+          } else {
+            $(`form #${param_id}`).val($(this).val())
+          }
+          $('.controls').change()
+        })
+      }).popover({
+        html: true,
+        sanitize: false,
+        title: render_popover_title,
+        content: render_popover_content
+      })
       function render() {
         $template.template({ row: data[index++], data: data, simulate, index, charts })
         if (index < data.length)
